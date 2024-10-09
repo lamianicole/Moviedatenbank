d3d26202e39a76b914d99c7c8d9158e6bdd8f885
Lamia Rizk <lamiarizk@gmx.de>
2024-10-09 11:51:46 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import './style.css'
+
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="./src/movies/movies.html">StartHere</a>
+  </div>
+`
+
+
---
+++ src/movies/movies.ts
@@ -0,0 +1,5 @@
+import { movies } from "./database";
+console.log(movies);
+
+
+
